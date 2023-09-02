eb8aeedd1706e0999aae1eb6a4ce35b3b78949d7
manishganatra <manishpganatra@gmail.com>
2023-09-02 00:24:35 -0500

    password application

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def home():
     if request.method == 'POST':
-        name = request.form['name']
-        return f'Hello, {name}!'
+        password = request.form['password']
+        if password == '1000':
+            return redirect(url_for('form'))
     return render_template_string('''
         <form method="post">
-            Name: <input type="text" name="name">
-            <input type="submit">
+            Password: <input type="password" name="password">
+            <input type="submit" value="Submit">
+        </form>
+    ''')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        return f'''
+            <h1>Your Submitted Information</h1>
+            <p>Name: {name}</p>
+            <p>Email: {email}</p>
+            <p>Phone: {phone}</p>
+        '''
+    return render_template_string('''
+        <form method="post">
+            Name: <input type="text" name="name"><br>
+            Email: <input type="email" name="email"><br>
+            Phone: <input type="tel" name="phone"><br>
+            <input type="submit" value="Submit">
         </form>
     ''')
 
