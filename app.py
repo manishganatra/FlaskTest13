d9b4d8502d3ed0cb93f20c6b712b627a5294168a
manishganatra <manishpganatra@gmail.com>
2023-09-02 00:07:56 -0500

    added avani

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! Manish'
+    return 'Hello, World! Manish and Avani'
 
 if __name__ == '__main__':
     app.run()
