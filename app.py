59be3cd86b2ebb091673e79924460e29884af1ee
manishganatra <manishpganatra@gmail.com>
2023-09-02 01:20:53 -0500

    excel download

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template_string, redirect, url_for
+from flask import Flask, request, render_template_string, redirect, url_for, send_file
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -21,12 +22,20 @@ def form():
         name = request.form['name']
         email = request.form['email']
         phone = request.form['phone']
-        return f'''
-            <h1>Your Submitted Information</h1>
-            <p>Name: {name}</p>
-            <p>Email: {email}</p>
-            <p>Phone: {phone}</p>
-        '''
+        
+        # Create a DataFrame
+        df = pd.DataFrame({
+            'Name': [name],
+            'Email': [email],
+            'Phone': [phone]
+        })
+
+        # Save to Excel
+        excel_filename = "output.xlsx"
+        df.to_excel(excel_filename, index=False, engine='xlsxwriter')
+
+        return send_file(excel_filename, as_attachment=True)
+
     return render_template_string('''
         <form method="post">
             Name: <input type="text" name="name"><br>
@@ -38,3 +47,4 @@ def form():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
