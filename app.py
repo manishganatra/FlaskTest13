dd8ba89b5b0b06b96ff9b36a164a9b3793426d9a
manishganatra <manishpganatra@gmail.com>
2023-09-02 00:15:58 -0500

    added textbox and change applicaton

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World! Manish and Avani'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Hello, {name}!'
+    return render_template_string('''
+        <form method="post">
+            Name: <input type="text" name="name">
+            <input type="submit">
+        </form>
+    ''')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
